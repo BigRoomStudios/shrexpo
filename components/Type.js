0fea7822d0d9bc1b7a76ec6b126a2049d3f8e1ad
Derek Wheelden <derek.wheelden@gmail.com>
2021-04-22 21:57:17 -0400

    Add HOC views

--- components/Type.js
+++ components/Type.js
@@ -1,31 +1,44 @@
 const { default: Styled } = require('styled-components/native');
 
-exports.Text = Styled.Text`
-    font-family: ${({ theme }) => theme.typography.primary};
-    font-size: ${({ theme }) => theme.spacing(1)}px;
-    color: ${({ theme }) => theme.palette.etch};
+const getBaseStyles = (theme) => {
+
+    return `
+        font-family: 'OpenSans_400Regular';
+        font-size: ${theme.spacing(1)}px;
+        color: ${theme.palette.etch};
+    `;
+};
+
+exports.getBaseStyles = getBaseStyles;
+
+exports.Text = Styled.Text`${({ theme }) => getBaseStyles(theme)}`;
+
+exports.CenteredText = Styled.Text`
+    ${({ theme }) => getBaseStyles(theme)}
+    margin-left: auto;
+    margin-right: auto;
 `;
 
 exports.H1 = Styled.Text`
+    ${({ theme }) => getBaseStyles(theme)}
     margin-bottom: ${({ theme }) => theme.spacing(2)}px;
-    font-family: ${({ theme }) => theme.typography.primary};
+    font-family: 'OpenSans_700Bold';
     font-size: ${({ theme }) => theme.spacing(2)}px;
     font-weight: bold;
-    color: ${({ theme }) => theme.palette.etch};
 `;
 
 exports.H2 = Styled.Text`
+    ${({ theme }) => getBaseStyles(theme)}
     margin-bottom: ${({ theme }) => theme.spacing(1.5)}px;
-    font-family: ${({ theme }) => theme.typography.primary};
-    font-size: ${({ theme }) => theme.spacing(2)}px;
+    font-family: 'OpenSans_700Bold';
+    font-size: ${({ theme }) => theme.spacing(1.5)}px;
     font-weight: bold;
-    color: ${({ theme }) => theme.palette.etch};
 `;
 
 exports.H3 = Styled.Text`
+    ${({ theme }) => getBaseStyles(theme)}
     margin-bottom: ${({ theme }) => theme.spacing(1.2)}px;
-    font-family: ${({ theme }) => theme.typography.primary};
-    font-size: ${({ theme }) => theme.spacing(2)}px;
+    font-family: 'OpenSans_700Bold';
+    font-size: ${({ theme }) => theme.spacing(1.2)}px;
     font-weight: bold;
-    color: ${({ theme }) => theme.palette.etch};
 `;
--- routes/demo/components/Demo.js
+++ routes/demo/components/Demo.js
@@ -1,14 +1,16 @@
 const React = require('react');
+const { useState } = require('react');
 const T = require('prop-types');
 const { default: Styled } = require('styled-components/native');
-const { Button } = require('react-native');
-const { Text } = require('components/Type');
+const { Button, TextInput, TouchableWithoutFeedback, Keyboard } = require('react-native');
+const { Text, CenteredText, H2, getBaseStyles } = require('components/Type');
 
 const CenteredView = Styled.View`
+    width: 100%;
     margin: auto;
 `;
 
-const Counter = Styled(Text)`
+const Heading = Styled(H2)`
     margin: ${({ theme }) => theme.spacing(2)}px auto;
     font-size: ${({ theme }) => theme.spacing(1.5)}px;
 `;
@@ -17,24 +19,42 @@ const ButtonWrapper = Styled.View`
     margin-bottom: ${({ theme }) => theme.spacing(1)}px;
 `;
 
+const InputWrapper = Styled(TextInput)`
+    ${({ theme }) => getBaseStyles(theme)}
+    width: 90%;
+    margin: ${({ theme }) => theme.spacing(1)}px auto;
+    padding: ${({ theme }) => theme.spacing(1)}px;
+    border: 1px solid
+`;
+
 module.exports = function Demo({ counter, increment, double }) {
 
+    const [input, setInput] = useState('');
+
     return (
-        <CenteredView>
-            <Counter>Counter: {counter}</Counter>
-            <ButtonWrapper>
-                <Button
-                    title="Add 1"
-                    onPress={increment}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <CenteredView>
+                <Heading>Counter: {counter}</Heading>
+                <ButtonWrapper>
+                    <Button
+                        title="Add 1"
+                        onPress={increment}
+                    />
+                </ButtonWrapper>
+                <ButtonWrapper>
+                    <Button
+                        title="Double"
+                        onPress={double}
+                    />
+                </ButtonWrapper>
+                <Heading>Type somethin'</Heading>
+                <InputWrapper
+                    value={input}
+                    onChangeText={setInput}
                 />
-            </ButtonWrapper>
-            <ButtonWrapper>
-                <Button
-                    title="Double"
-                    onPress={double}
-                />
-            </ButtonWrapper>
-        </CenteredView>
+                {!!input && <CenteredText>Hey you typed {input}!</CenteredText>}
+            </CenteredView>
+        </TouchableWithoutFeedback>
     );
 };
 
