5722196ac6b222c6beb14a50c680588e04167313
Tyler Constance <tyler@tylerconstance.com>
2023-01-23 14:25:52 -0500

    Adds logout action and adjusts type size

--- middle-end/auth/action-types.js
+++ middle-end/auth/action-types.js
@@ -3,5 +3,6 @@ const MiddleEnd = require('strange-middle-end');
 module.exports = MiddleEnd.createTypes('auth', {
     FETCH_CURRENT_USER: MiddleEnd.type.async,
     LOGIN: MiddleEnd.type.async,
+    LOGOUT: MiddleEnd.type.async,
     REGISTER: MiddleEnd.type.async
 });
